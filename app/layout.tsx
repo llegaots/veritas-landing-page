1b43a16615f6275d2f49f605290ac12e0e91e644
Lucas Legatos <lucaslegatos123@gmail.com>
2026-02-05 09:04:08 -0500

    Add Facebook Pixel (Meta Pixel) to landing page

---
+++ app/layout.tsx
@@ -0,0 +1,63 @@
+import type { Metadata } from "next";
+import { Montserrat, Inter } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Horizon Park Apartments - Investment Opportunity | Veritas Equity Partners",
+  description: "Rare opportunity to invest in a 36-unit apartment complex in Edmonds, WA. $50K minimum investment with 2.22x equity multiple and 18.1% target annual IRR. Accredited investors only.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '465177802948503');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=465177802948503&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+      <body className={`${montserrat.variable} ${inter.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
